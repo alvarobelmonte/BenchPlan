ddc764078fb84b4959961c6859e16896d6bad24d
aba93 <abelmonte4eso2@gmail.com>
2016-02-09 17:12:18 +0100

    Editar jugador (sin terminar)

---
+++ www/views/playerDetail/playerDetail.js
@@ -0,0 +1,71 @@
+'Use Strict';
+angular.module('App').controller('playerDetailController', function (APIfactory, $scope, $state,$cordovaOauth, $localStorage, $location,$http,$ionicPopup, $firebaseObject, $firebaseArray, Auth, FURL, Utils) {
+  
+  var ref = new Firebase(FURL);
+
+  //Dorsales
+  $scope.dorsals = [];
+  var initDorsals = function() {
+    var i;
+    for (i = 0;i <= 27; i++) {
+      $scope.dorsals.push(i);
+    }
+  }
+  initDorsals();
+
+  $scope.nombre = "nombre";
+  $scope.posicion = "posicion";
+  $scope.whichplayer = $state.params.aId;
+  $scope.editing = false;
+
+  $scope.toggleInjured = function(player){
+  	player.star = !player.star;
+  }
+
+  $scope.showP = function () {
+	    $scope.players = APIfactory.getJugadores();
+	    console.log($scope.players);
+  }
+
+  $scope.showP();
+
+  $scope.onItemDelete = function (player) {
+	    APIfactory.deleteJugador(player);
+  }
+
+  $scope.turnFalse = function () {
+      console.log('showForm');
+      $scope.editing = true;
+  }
+
+  $scope.editP = function (player) {
+    console.log("Enviada peticion editar jugador");
+
+    //Recogemos datos del formulario
+    $scope.name = player.name;
+    $scope.position = player.position;
+    $scope.dorsal = player.dorsal;
+    $scope.fecha = player.fecha;
+    $scope.estado = player.condition;
+    var id = player.$id;
+    
+    //Referencia a la rama players del usuario que ha iniciado sesion
+    var userRef = ref.child('profile').child($localStorage.userkey).child("player").child(player.$id);
+
+    
+    console.log(player.name);
+
+    //APIfactory.editJugador(player, id);
+    APIfactory.editJugador(player, id);
+    //Resetear formulario
+
+
+    //Pop up de confirmación
+    var alertPopup = $ionicPopup.alert({
+       title: 'Jugador editado',
+       /*template: $scope.name*/
+     });
+
+  }
+}
+);
