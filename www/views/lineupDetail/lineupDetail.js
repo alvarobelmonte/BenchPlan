e98c1c9db2635aac613f3f88aaf128391e5a41f0
aba93 <abelmonte4eso2@gmail.com>
2016-06-09 22:45:58 +0200

    Posiciones según formación (Alineaciones)

--- www/views/lineupDetail/lineupDetail.js
+++ www/views/lineupDetail/lineupDetail.js
@@ -22,16 +22,16 @@ angular.module('App').controller('lineupDetailController', function (APIfactory,
     $scope.p11pos = 'delantero';
 
     //Clases
-    $scope.p2class = 'def df4p1';
-    $scope.p3class = 'def df4p2';
-    $scope.p4class = 'def df4p3';
-    $scope.p5class = 'def df4p4';
-    $scope.p6class = 'mc1 m4p1';
-    $scope.p7class = 'mc1 m4p2';
-    $scope.p8class= 'mc1 m4p3';
-    $scope.p9class = 'mc1 m4p4';
-    $scope.p10class = 'del dl2p1';
-    $scope.p11class = 'del dl2p2';
+    $scope.p2class = 'def linea4p1';
+    $scope.p3class = 'def linea4p2';
+    $scope.p4class = 'def linea4p3';
+    $scope.p5class = 'def linea4p4';
+    $scope.p6class = 'mc1 linea4p1';
+    $scope.p7class = 'mc1 linea4p2';
+    $scope.p8class= 'mc1 linea4p3';
+    $scope.p9class = 'mc1 linea4p4';
+    $scope.p10class = 'del linea2p1';
+    $scope.p11class = 'del linea2p2';
 
     var alineado = false;
 
@@ -86,30 +86,30 @@ angular.module('App').controller('lineupDetailController', function (APIfactory,
       $scope.dm2formado = medio;
       $scope.p5pos = 'medio';
       $scope.p6pos = 'medio';
-      $scope.p2class = 'def df3p1';
-      $scope.p3class = 'def df3p2';
-      $scope.p4class = 'def df3p3';
+      $scope.p2class = 'def linea3p1';
+      $scope.p3class = 'def linea3p2';
+      $scope.p4class = 'def linea3p3';
     }
     else if($scope.defensas == '4'){
       $scope.dmformado = defensa;
       $scope.dm2formado = medio;
       $scope.p5pos = 'defensa';
       $scope.p6pos = 'medio';
-      $scope.p2class = 'def df4p1';
-      $scope.p3class = 'def df4p2';
-      $scope.p4class = 'def df4p3';
-      $scope.p5class = 'def df4p4';
+      $scope.p2class = 'def linea4p1';
+      $scope.p3class = 'def linea4p2';
+      $scope.p4class = 'def linea4p3';
+      $scope.p5class = 'def linea4p4';
     }
     else if($scope.defensas == '5'){
       $scope.dmformado = defensa;
       $scope.dm2formado = defensa;
       $scope.p5pos = 'defensa';
       $scope.p6pos = 'defensa';
-      $scope.p2class = 'def df5p1';
-      $scope.p3class = 'def df5p2';
-      $scope.p4class = 'def df5p3';
-      $scope.p5class = 'def df5p4';
-      $scope.p6class = 'def df5p5';
+      $scope.p2class = 'def linea5p1';
+      $scope.p3class = 'def linea5p2';
+      $scope.p4class = 'def linea5p3';
+      $scope.p5class = 'def linea5p4';
+      $scope.p6class = 'def linea5p5';
     }
 
     //Medios y delanteros
@@ -118,20 +118,64 @@ angular.module('App').controller('lineupDetailController', function (APIfactory,
       $scope.md2formado = medio;
       $scope.p9pos = 'medio';
       $scope.p10pos = 'medio';
+
+      $scope.p2class = 'def linea4p1';
+      $scope.p3class = 'def linea4p2';
+      $scope.p4class = 'def linea4p3';
+      $scope.p5class = 'def linea4p4';
+      $scope.p11class = 'del linea1p1';
+
+      if($scope.medios2 == '2'){
+          $scope.p6class = 'mc1 linea3p1';
+          $scope.p7class = 'mc1 linea3p2';
+          $scope.p8class = 'mc1 linea3p3';
+          $scope.p9class = 'mc2 linea2p1';
+          $scope.p10class = 'mc2 linea2p2';
+      }
+      else if($scope.medios2 == '3'){
+          $scope.p6class = 'mc1 linea2p1';
+          $scope.p7class = 'mc1 linea2p2';
+          $scope.p8class = 'mc2 linea3p1';
+          $scope.p9class = 'mc2 linea3p2';
+          $scope.p10class = 'mc2 linea3p3';
+      }
     }
     else if($scope.delanteros == '2'){
       $scope.md1formado = medio;
       $scope.md2formado = delantero;
       $scope.p9pos = 'medio';
       $scope.p10pos = 'delantero';
-      $scope.p10class = 'del dl2p1';
-      $scope.p11class = 'del dl2p2';
+
+      $scope.p2class = 'def linea4p1';
+      $scope.p3class = 'def linea4p2';
+      $scope.p4class = 'def linea4p3';
+      $scope.p5class = 'def linea4p4';
+
+      
+      $scope.p6class = 'mc1 linea3p1';
+      $scope.p7class = 'mc1 linea3p2';
+      $scope.p8class = 'mc1 linea3p3';
+      $scope.p9class = 'mc2 linea1p1';
+      $scope.p10class = 'del linea2p1';
+      $scope.p11class = 'del linea2p2';
     }
     else if($scope.delanteros == '3'){
       $scope.md1formado = delantero;
       $scope.md2formado = delantero;
       $scope.p9pos = 'delantero';
       $scope.p10pos = 'delantero';
+
+      $scope.p2class = 'def linea4p1';
+      $scope.p3class = 'def linea4p2';
+      $scope.p4class = 'def linea4p3';
+      $scope.p5class = 'def linea4p4';
+
+      $scope.p6class = 'mc1 linea2p1';
+      $scope.p7class = 'mc1 linea2p2';
+      $scope.p8class = 'mc2 linea1p1';
+      $scope.p9class = 'del linea3p1';
+      $scope.p10class = 'del linea3p2';
+      $scope.p11class = 'del linea3p3';
     }
     else if($scope.delanteros == ''){
       if($scope.medios2 == '1'){
@@ -139,18 +183,97 @@ angular.module('App').controller('lineupDetailController', function (APIfactory,
         $scope.md2formado = medio;
         $scope.p9pos = 'medio';
         $scope.p10pos = 'medio';
+        $scope.p11class = 'mc2 linea1p1';
+      
+        if($scope.medios1 == '5'){
+          $scope.p2class = 'def linea4p1';
+          $scope.p3class = 'def linea4p2';
+          $scope.p4class = 'def linea4p3';
+          $scope.p5class = 'def linea4p4';
+          $scope.p6class = 'mc1 linea5p1';
+          $scope.p7class = 'mc1 linea5p2';
+          $scope.p8class = 'mc1 linea5p3';
+          $scope.p9class = 'mc1 linea5p4';
+          $scope.p10class = 'mc1 linea5p5';
+        }
+        else if($scope.medios1 == '4'){
+          $scope.p2class = 'def linea5p1';
+          $scope.p3class = 'def linea5p2';
+          $scope.p4class = 'def linea5p3';
+          $scope.p5class = 'def linea5p4';
+          $scope.p6class = 'def linea5p5';
+          $scope.p7class = 'mc1 linea4p1';
+          $scope.p8class = 'mc1 linea4p2';
+          $scope.p9class = 'mc1 linea4p3';
+          $scope.p10class = 'mc1 linea4p4';
+        }
+
       }
       else if($scope.medios2 == '2'){
         $scope.md1formado = medio;
         $scope.md2formado = delantero;
         $scope.p9pos = 'medio';
         $scope.p10pos = 'delantero';
+        $scope.p10class = 'mc2 linea2p1';
+        $scope.p11class = 'mc2 linea2p2';
+          if($scope.medios1 == '5'){
+            $scope.p2class = 'def linea3p1';
+            $scope.p3class = 'def linea3p2';
+            $scope.p4class = 'def linea3p3';
+            $scope.p5class = 'mc1 linea5p1';
+            $scope.p6class = 'mc1 linea5p2';
+            $scope.p7class = 'mc1 linea5p3';
+            $scope.p8class = 'mc1 linea5p4';
+            $scope.p9class = 'mc1 linea5p5';
+          }
+          else if($scope.medios1 == '4'){
+            $scope.p2class = 'def linea4p1';
+            $scope.p3class = 'def linea4p2';
+            $scope.p4class = 'def linea4p3';
+            $scope.p5class = 'def linea4p4';
+            $scope.p6class = 'mc1 linea4p1';
+            $scope.p7class = 'mc1 linea4p2';
+            $scope.p8class = 'mc1 linea4p3';
+            $scope.p9class = 'mc1 linea4p4';
+          }
+          else if($scope.medios1 == '3'){
+            $scope.p2class = 'def linea5p1';
+            $scope.p3class = 'def linea5p2';
+            $scope.p4class = 'def linea5p3';
+            $scope.p5class = 'def linea5p4';
+            $scope.p6class = 'def linea5p5';
+            $scope.p7class = 'mc1 linea3p1';
+            $scope.p8class = 'mc1 linea3p2';
+            $scope.p9class = 'mc1 linea3p3';
+          }
       }
       else if($scope.medios2 == '3'){
         $scope.md1formado = delantero;
         $scope.md2formado = delantero;
         $scope.p9pos = 'delantero';
         $scope.p10pos = 'delantero';
+        $scope.p9class = 'mc2 linea3p1';
+        $scope.p10class = 'mc2 linea3p2';
+        $scope.p11class = 'mc2 linea3p3';
+
+        if($scope.medios1 == '4'){
+            $scope.p2class = 'def linea3p1';
+            $scope.p3class = 'def linea3p2';
+            $scope.p4class = 'def linea3p3';
+            $scope.p5class = 'mc1 linea4p1';
+            $scope.p6class = 'mc1 linea4p2';
+            $scope.p7class = 'mc1 linea4p3';
+            $scope.p8class = 'mc1 linea4p4';
+          }
+          else if($scope.medios1 == '3'){
+            $scope.p2class = 'def linea4p1';
+            $scope.p3class = 'def linea4p2';
+            $scope.p4class = 'def linea4p3';
+            $scope.p5class = 'def linea4p4';
+            $scope.p6class = 'mc1 linea3p1';
+            $scope.p7class = 'mc1 linea3p2';
+            $scope.p8class = 'mc1 linea3p3';
+          }
       }
     }
 
