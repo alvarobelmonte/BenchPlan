8bd75eeef7a6929e7b307d6a3939de38a7eef7ac
Alvaro Belmonte <abelmonte4eso2@gmail.com>
2017-11-28 12:20:05 +0100

    Changed style on login

--- www/views/login/login.js
+++ www/views/login/login.js
@@ -1,54 +1,74 @@
-'Use Strict';
-angular.module('App').controller('loginController', function ($scope, $state,$cordovaOauth, $localStorage, $location,$http,$ionicPopup, $firebaseObject, Auth, FURL, Utils) {
-  var ref = new Firebase(FURL);
-  var userkey = "";
-  $scope.signIn = function (user) {
-    console.log("Enviado");
-    if(angular.isDefined(user)){
-    Utils.show();
-    Auth.login(user)
-      .then(function(authData) {
-      //console.log("id del usuario:" + JSON.stringify(authData));
+"Use Strict";
+angular
+  .module("App")
+  .controller("loginController", function(
+    $scope,
+    $ionicSideMenuDelegate,
+    $state,
+    $cordovaOauth,
+    $localStorage,
+    $location,
+    $http,
+    $ionicPopup,
+    $firebaseObject,
+    Auth,
+    FURL,
+    Utils
+  ) {
+    var ref = new Firebase(FURL);
+    var userkey = "";
+    $scope.signIn = function(user) {
+      console.log("Enviado");
+      if (angular.isDefined(user)) {
+        Utils.show();
+        Auth.login(user).then(
+          function(authData) {
+            //console.log("id del usuario:" + JSON.stringify(authData));
 
-      ref.child('profile').orderByChild("id").equalTo(authData.uid).on("child_added", function(snapshot) {
-        console.log(snapshot.key());
-        userkey = snapshot.key();
-        var obj = $firebaseObject(ref.child('profile').child(userkey));
+            ref
+              .child("profile")
+              .orderByChild("id")
+              .equalTo(authData.uid)
+              .on("child_added", function(snapshot) {
+                console.log(snapshot.key());
+                userkey = snapshot.key();
+                var obj = $firebaseObject(ref.child("profile").child(userkey));
 
-        obj.$loaded()
-          .then(function(data) {
-            //console.log(data === obj); // true
-            //console.log(obj.email);
-            $localStorage.email = obj.email;
-            $localStorage.userkey = userkey;
+                obj
+                  .$loaded()
+                  .then(function(data) {
+                    //console.log(data === obj); // true
+                    //console.log(obj.email);
+                    $localStorage.email = obj.email;
+                    $localStorage.userkey = userkey;
 
-              Utils.hide();
-              $state.go('tabs.players');
-              console.log("Starter page","Home");
+                    Utils.hide();
+                    $state.go("tabs.players");
+                    console.log("Starter page", "Home");
+                  })
+                  .catch(function(error) {
+                    console.error("Error:", error);
+                  });
+              });
+          },
+          function(err) {
+            Utils.hide();
+            Utils.errMessage(err);
+          }
+        );
+      }
+    };
 
-          })
-          .catch(function(error) {
-            console.error("Error:", error);
-          });
-      });
+    $scope.checkLocalStorage = function() {
+      if (
+        $localStorage.email !== undefined &&
+        $localStorage.userkey !== undefined
+      ) {
+        $state.go("tabs.players");
+      }
+    };
 
-      }, function(err) {
-        Utils.hide();
-         Utils.errMessage(err);
-      });
-    }
-  };
+    $scope.checkLocalStorage();
 
-  $scope.checkLocalStorage = function () {
-    if ($localStorage.email  !== undefined && $localStorage.userkey !== undefined) {
-            $state.go('tabs.players');
-        }
-      
-  };
-
-  $scope.checkLocalStorage();
-
-
-
-
-});
+    $ionicSideMenuDelegate.canDragContent(false); //Disables side menu
+  });
